078cd5e6c33c139076ce50f3632aa6edc8a28abf
Alone2 <alain.sinzig@gmail.com>
2020-04-16 01:41:27 +0200

    video more tello-like

--- startvideo.sh
+++ startvideo.sh
@@ -1,2 +1,6 @@
+# ffmpeg -r 25 -re -f lavfi -i testsrc=duration=30:size=960x720:rate=25 -vf "fps=30" -f h264 udp://0.0.0.0:11111 
+
 # Video
-ffmpeg -f lavfi -i testsrc=duration=100:size=1280x720:rate=60  -f mpegts udp://0.0.0.0:11111
+ffmpeg -r 25 -re -f lavfi -i testsrc=duration=30:size=960x720:rate=25 -f h264 udp://0.0.0.0:11111 
+
+# ffmpeg -r 25 -re -f lavfi -i testsrc=duration=3600:size=960x720:rate=25  -c:v libx265 -preset ultrafast -x265-params crf=23 -strict experimental -f h264 udp://0.0.0.0:11111 
